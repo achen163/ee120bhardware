6c58b7ba01251d47a7e227cc5ce5eff9575c0de6
Alex Chen <achen163@ucr.edu>
2020-05-22 21:27:46 -0700

    p2

--- Lab10/turnin/achen163_lab10_part2.c
+++ Lab10/turnin/achen163_lab10_part2.c
@@ -1,7 +1,7 @@
 /*	Author: Alex Chen
  *  Partner(s) Name: 
  *	Lab Section: 22
- *	Assignment: Lab #10  Exercise #1
+ *	Assignment: Lab #10  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
